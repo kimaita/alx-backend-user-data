869ef58cb43d28cb085e61c20252a3921a3e0bb8
kimaita <johnnieqaym@gmail.com>
2024-09-17 15:55:30 +0300

    added base64 string extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,18 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Defines methods for implemmenting HTTP Authentication"""
 
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str,
+    ) -> str:
+        """Returns the Base64 part of the Authorization header
+        for Basic Authentication"""
+
+        if not authorization_header or type(authorization_header) != str:
+            return None
+
+        tag = "Basic "
+        if authorization_header.startswith(tag):
+            return authorization_header[len(tag) :]
+
+        return None
