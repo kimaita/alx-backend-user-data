103df805002daeae6a66aa7d3a9ef5fd68f947f5
kimaita <johnnieqaym@gmail.com>
2024-09-20 11:25:36 +0300

    added session authentication class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,6 +42,10 @@ elif auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
 
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+
+    auth = SessionAuth()
 
 
 @app.before_request
