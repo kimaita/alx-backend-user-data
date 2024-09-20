ba2db05989072cf68ad313027825ab2c09d73cf7
kimaita <johnnieqaym@gmail.com>
2024-09-20 10:22:08 +0300

    rewrite function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Implements API HTTP authentication"""
 
-from flask import request
 from typing import List, TypeVar
-#from pathlib import PurePath
 
 
 class Auth:
@@ -23,10 +21,12 @@ class Auth:
 
         path += "/" if not path.endswith("/") else ""
 
-        if path in excluded_paths:
-            return False
+        for excl in excluded_paths:
+            if excl == path or \
+                (excl.endswith("*") and path.startswith(excl[:-1])):
+                return False
 
-        #return all(not PurePath(path).match(excl) for excl in excluded_paths)
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns the request's `Authorization Header` or None"""
