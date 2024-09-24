6296caa422e24eba65733b2578b10ab865c3571e
kimaita <johnnieqaym@gmail.com>
2024-09-25 01:47:55 +0300

    added login API endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Flask server"""
 
-from flask import jsonify, Flask, request
-from auth import Auth
+from flask import Flask, abort, jsonify, request
 
+from auth import Auth
 
 AUTH = Auth()
 app = Flask(__name__)
@@ -24,10 +24,22 @@ def register_user():
         user = AUTH.register_user(email, pwd)
         if user:
             return jsonify({"email": email, "message": "user created"})
-    except KeyError:
-        return
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login_user():
+    """Creates a new session for the user"""
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+    if AUTH.valid_login(email, pwd):
+        session_id = AUTH.create_session(email)
+        resp = jsonify({"email": email, "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+        return resp
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
