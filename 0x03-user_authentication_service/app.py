c620de22ad2828a4662abba738fe5794a03a8351
kimaita <johnnieqaym@gmail.com>
2024-09-25 03:16:28 +0300

    added password reset

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,5 +65,32 @@ def get_user():
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """Endpoint for getting a user password reset token"""
+    email = request.form.get("email")
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """Endpoint for updating a user password.
+
+    Requires the form fields: "email", "reset_token", "new_password"
+    """
+    try:
+        email = request.form.get("email")
+        token = request.form.get("reset_token")
+        pwd = request.form.get("new_password")
+        AUTH.update_password(token, pwd)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,3 +90,25 @@ class Auth:
         """
         # if isinstance(user_id, int):
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a password reset token for a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates a user's password.
+
+        The reset_token is used to identify the user.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
+        except NoResultFound:
+            raise ValueError
