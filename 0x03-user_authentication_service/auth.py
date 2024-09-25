290e1201551c977c6686414c9c4104a8efc6d52f
kimaita <johnnieqaym@gmail.com>
2024-09-25 03:18:34 +0300

    line too long :/

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,6 +109,10 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             pwd = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
+            self._db.update_user(
+                user.id,
+                hashed_password=pwd,
+                reset_token=None,
+            )
         except NoResultFound:
             raise ValueError
