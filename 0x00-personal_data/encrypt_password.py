8d80ed2e3c047254cb99d062acca1eff2a875bdf
kimaita <johnnieqaym@gmail.com>
2024-11-20 17:09:55 +0300

    added password hashing module

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Password hashing"""
+
+import bcrypt
+
+
+def hash_passsword(password: str) -> bytes:
+    """Returns a salted hash of `password`
+
+    Args:
+        password (str): password to hash
+
+    Returns:
+        bytes: salted, hashed password
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that `password` matches `hashed_password`
+
+    Args:
+        hashed_password (bytes): password hash
+        password (str): password string
+
+    Returns:
+        bool: True if a match, False otherwise
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
