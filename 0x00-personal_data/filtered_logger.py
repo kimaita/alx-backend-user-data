05f58adf5edd151849d19b5bd16c6adf532efb15
kimaita <johnnieqaym@gmail.com>
2024-11-20 16:50:12 +0300

    fix: default database name

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -76,7 +76,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
         user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
         password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-        database=os.getenv("PERSONAL_DATA_DB_NAME"),
+        database=os.getenv("PERSONAL_DATA_DB_NAME", "holberton"),
     )
 
 
